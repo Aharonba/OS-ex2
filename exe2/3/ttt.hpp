906bec28475fa07f05547357380a7509b5000b89
Aharonba <146740959+Aharonba@users.noreply.github.com>
2024-07-01 14:03:19 +0000

    Delete exe2 directory

--- exe2/3/ttt.hpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cstdlib>
-#include <cstring>
-#include <vector>
-
-
-bool isValid(const char* str);
-bool gameOver(int arr[3][3], int* winner);
-bool putBoardPosition(int number, int arr[3][3], int player);
